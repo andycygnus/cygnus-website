caf641b4b55f0107ea4dec7ebb330f9266e58a44
andycygnus <andy.benavides@cygnuspay.com>
2023-03-15 14:14:01 -0400

    Update team.jsx

--- src/components/home/team.jsx
+++ src/components/home/team.jsx
@@ -1,5 +1,4 @@
 import team_home_two from "@/data/team-home-2";
-import { bottom } from "@popperjs/core";
 import React, { useEffect, useState } from "react";
 import { Navigation } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -49,12 +48,50 @@ const Team = () => {
         <div className="container" style={{textAlign: 'center'}}>
           <span className="tp-section__sub-title left-line right-line mb-25">Our Services</span>
         </div>
-        <div className="container" style={{marginBottom: '60px'}}>
+        <div className="container" style={{marginBottom: '80px'}}>
           <div className="p-relative">
             <div className="row">
               <div className="col-lg-12">
                 <div className="tp-section text-center">
                   <h3 className="tp-section__title" style={{textAlign: 'left', paddingLeft: '30px'}}>POS Solutions</h3>
+                </div>
+              </div>
+            </div>
+            <Swiper
+              {...setting}
+              modules={[Navigation]}
+              loop={isLoop}
+              className="swiper-container tp-team-active wow fadeInUp"
+              data-wow-delay=".3s"
+            >
+              {team_home_two.map((item) => (
+                <SwiperSlide key={item.id}>
+                  <div className="swiper-slide">
+                    <div className="team-item mb-30">
+                      <div className="team-item__thumb mb-40">
+                        <img src={item.img} alt="team-thumb" />
+                      </div>
+                    </div>
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+            <div className="team-arrow">
+              <div className="team-prv">
+                <i className="fa-regular fa-arrow-left"></i>
+              </div>
+              <div className="team-nxt">
+                <i className="fa-regular fa-arrow-right"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="container" style={{marginBottom: '80px'}}>
+          <div className="p-relative">
+            <div className="row">
+              <div className="col-lg-12">
+                <div className="tp-section text-center">
+                  <h3 className="tp-section__title" style={{textAlign: 'left', paddingLeft: '30px'}}>Terminal Solutions</h3>
                 </div>
               </div>
             </div>
