c0bc9d0be58b06192029b83cb28c81be178ffd6a
mikekim624 <54910278+mikekim624@users.noreply.github.com>
2023-03-25 00:44:31 -0400

    Update hero-banner.jsx

--- src/components/home/hero-banner.jsx
+++ src/components/home/hero-banner.jsx
@@ -1,108 +1,106 @@
-import Link from "next/link";
-import React, { useEffect, useState } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation } from "swiper";
+import Link from 'next/link'
+import React, { useEffect, useState } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation } from 'swiper'
 
 const hero_slider = [
-  {
-    id: 1,
-    title: (
-      <>
-        Cygnus Payments<br/> Is Easy to Work With!
-      </>
-    ),
-    subtitle: "Honesty, Transparency & Convenience",
-    des: (
-      <>
-        We make it easy for you to accept payments with flexible options<br/> and competitive rates. We have the solution for you.
-      </>
-    ),
-    appointment: "Get a Quote",
-    about: "About us",
-    img_1: "/assets/img/slider/slider-bg-3.png",
-    img_2: "/assets/img/slider/slider-bg-4.png",
-  },
-];
+    {
+        id: 1,
+        title: (
+            <>
+                Cygnus Payments
+                <br /> Is Easy to Work With!
+            </>
+        ),
+        subtitle: 'Honesty, Transparency & Convenience',
+        des: (
+            <>
+                We make it easy for you to accept payments with flexible options
+                <br /> and competitive rates. We have the solution for you.
+            </>
+        ),
+        appointment: 'Get a Quote',
+        about: 'About us',
+        img_1: '/assets/img/slider/slider-bg-3.png',
+        img_2: '/assets/img/slider/slider-bg-4.png',
+    },
+]
 
 // slider setting
 const setting = {
-  // Optional parameters
-  slidesPerView: 1,
-  effect: "fade",
-  autoplay: {
-    delay: 4000,
-    disableOnInteraction: true,
-  },
-  navigation: {
-    nextEl: ".slider-n",
-    prevEl: ".slider-p",
-  },
-};
+    // Optional parameters
+    slidesPerView: 1,
+    effect: 'fade',
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: true,
+    },
+    navigation: {
+        nextEl: '.slider-n',
+        prevEl: '.slider-p',
+    },
+}
 
 // social_links
-const social_links = [
-];
+const social_links = []
 const HeroBanner = () => {
-  const [isLoop, setIsLoop] = useState(false);
-  useEffect(() => {
-    setIsLoop(true);
-  }, []);
-  return (
-    <>
-      <section className="slider-area slider-tp-top p-relative">
-        <div className="slider-social">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-6 col-md-10 d-none d-md-block">
-                <div className="slider-content__social">
-                  {social_links.map((link, i) => (
-                    <Link
-                      key={i}
-                      target={link.target}
-                      className={link.color}
-                      href={link.link}
-                    >
-                      <i className={link.icon}></i> {link.name}
-                    </Link>
-                  ))}
-                </div>
-              </div>
-              <div className="col-lg-6 col-md-2 col-12 d-none d-md-block">
-              </div>
-            </div>
-          </div>
-        </div>
-        {hero_slider.map((item) => (
-          <div className="swiper-slide bg-white mt-50">
-            <div className="container">
-              <div className="row align-items-center">
-                <div className="col-lg-6 col-md-12 col-12 order-2 order-lg-1">
-                  <div className="slider-content pt-60">
-                  <h2 className="slider-content__title mb-45" style={{ color: '#424143' }}>{item.title}</h2>
-                  <h3 className="slider-content__subtitle mb-30" style={{color: '#808285'}}>{item.subtitle}</h3>
-                  <p>{item.des}</p>
-                  <div className="slider-content__btn mb-40">
-                    <Link className="tp-btn" href="/quote">
-                      {item.appointment}
-                    </Link>
-                  </div>
-                  </div>
-                </div>
-                <div className="col-lg-6 col-md-6 col-12 order-1 order-lg-2">
-                  <div className="slider-content__bg">
-                    <img src={item.img_1} alt="slider-img" />
-                  </div>
-                  <div className="slider-content__shape d-none d-md-block">
-                    <img src={item.img_2} alt="slider-shape" />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        ))}
-      </section>
-    </>
-  );
-};
+    const [isLoop, setIsLoop] = useState(false)
+    useEffect(() => {
+        setIsLoop(true)
+    }, [])
+    return (
+        <>
+            <section className="slider-area slider-tp-top p-relative">
+                {hero_slider.map((item) => (
+                    <div className="swiper-slide bg-white mt-50">
+                        <div className="container">
+                            <div className="row align-items-center">
+                                <div className="col-lg-6 col-md-12 col-12 order-2 order-lg-1">
+                                    <div className="slider-content pt-60">
+                                        <h2
+                                            className="slider-content__title mb-45"
+                                            style={{ color: '#424143' }}
+                                        >
+                                            {item.title}
+                                        </h2>
+                                        <h3
+                                            className="slider-content__subtitle mb-30"
+                                            style={{ color: '#808285' }}
+                                        >
+                                            {item.subtitle}
+                                        </h3>
+                                        <p>{item.des}</p>
+                                        <div className="slider-content__btn mb-40">
+                                            <Link
+                                                className="tp-btn"
+                                                href="/quote"
+                                            >
+                                                {item.appointment}
+                                            </Link>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-lg-6 col-md-6 col-12 order-1 order-lg-2">
+                                    <div className="slider-content__bg">
+                                        <img
+                                            src={item.img_1}
+                                            alt="slider-img"
+                                        />
+                                    </div>
+                                    <div className="slider-content__shape d-none d-md-block">
+                                        <img
+                                            src={item.img_2}
+                                            alt="slider-shape"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </section>
+        </>
+    )
+}
 
-export default HeroBanner;
+export default HeroBanner
