5db89aac87ac8968c03fcf6e43e6e6efe9490532
mikekim624 <54910278+mikekim624@users.noreply.github.com>
2023-09-18 12:02:06 -0400

    Update privacy-policy.jsx

--- src/components/privacy/privacy-policy.jsx
+++ src/components/privacy/privacy-policy.jsx
@@ -10,46 +10,120 @@ const PrivacyPolicy = () => {
                             <div class="content-page">           
                                 <h2>Cygnus Payment Solutions Privacy Policy</h2>
                                 <h5><b>What Cygnus Payment Solutions Does with your Personal Information</b></h5>
-                                <p>This privacy policy discloses the privacy practices for (website address). This privacy policy applies solely to information collected by this web site. It will notify you of the following: </p>
-                                <ul>
-                                <li>What personally identifiable information is collected from you through the web site, how it is used and with whom it may be shared. </li>
-                                <li>What choices are available to you regarding the use of your data. </li>
-                                <li>The security procedures in place to protect the misuse of your information. </li>				  
-                                <li>How you can correct any inaccuracies in the information. </li>				  
-                                </ul>
+                                <p>Cygnus endeavors to protect the privacy and confidentiality of the personal date with which we are entrusted. 
+                                    This Privacy Policy Notice outlines the personal data we collect or process in connection with the services we provide or offer to you as a consumer, 
+                                    including through any site, application or product that links to this Privacy Notice (the “Service”), how we use, share, and protect that personal data.</p>
+                                <p>
+                                    Personal data is defined as any information that is linked or reasonably linkable to an identified or identifiable natural person. 
+                                    Personal data does not include information that has been de-identified or is publicly available.
+                                </p>
+
                                 <h5><b>Information Collection, Use, and Sharing</b></h5>
-                                    <p>We are the sole owners of the information collected on this site. We only have access to/collect information that you voluntarily give us via email or other direct contact from you. We will not sell or rent this information to anyone. </p>
-                                    <p>We will use your information to respond to you, regarding the reason you contacted us. We will not share your information with any third party outside of our organization, other than as necessary to fulfill your request, e.g. to ship an order. </p>
-                                    <p>Unless you ask us not to, we may contact you via email in the future to tell you about specials, new products or services, or changes to this privacy policy. </p>
+                                    <p>We collect only the minimum amount of information needed to provide services. 
+                                        If you do nothing during your visit to our website but browse or download information 
+                                        we automatically collect and store the following information about your visit:</p>
+                                    <ul>		
+                                    <li>1. The internet domain and IP address from which you accessed our site;</li>
+                                    <li>2. The type of browser and operating system you used;</li>
+                                    <li>3. The date and time you visited; and</li>
+                                    <li>4. If you arrived from another website, the address of that website.</li>
+                                    </ul>
+                                    <p>
+                                    We do collect personal information directly from individuals who volunteer to complete forms 
+                                    or participate in surveys, in order to deliver the services requested. We only collect, 
+                                    retain and use personal information where we believe it is essential to administer our 
+                                    business and to provide products, services and other opportunities requested by our customers.
+                                    </p>
+                                    <p>
+                                    We do not sell or rent our subscribers’/customers’ information to any outside company or organization. 
+                                    We do not reveal specific information about users or other personally identifiable data to 
+                                    unaffiliated third parties for their independent use, except if required to do so by FOIA or 
+                                    other law.
+                                    </p>
+                                    <p>
+                                    Unless you ask us not to, we may contact you via email in the future to tell you about specials, 
+                                    new products or services, or changes to this privacy policy.
+                                    </p>
                                     
                                 <h5><b>Your Access to and Control Over Information </b></h5>
                                     <p>You may opt out of any future contacts from us at any time. You can do the following at any time by contacting us via the email address or phone number given on our website: </p>	
                                     <ul>		
-                                    <li>See what data we have about you, if any.</li>
-                                    <li>Change/correct any data we have about you.</li>
-                                    <li>Have us delete any data we have about you.</li>
-                                    <li>Express any concern you have about our use of your data. </li>
+                                    <li>1. <b>Right to Confirm & Access:</b> You have the right to request confirmation from us as to whether or not we are processing your personal data and to access such personal data.</li>
+                                    <li>2. <b>Right to Request Correction of Inaccuracies:</b> You have the right to request that we correct any inaccuracies in any of your personal information, which is under our control, taking into account the nature of the data and the purposes of processing the data.</li>
+                                    <li>3. <b>Right to Request Deletion of Personal Data:</b> You have the right to request that we delete any of your personal data that we collected from or about you and retained.</li>
+                                    <li>4. <b>Right to Request Disclosure of Information:</b> You have the right to request a copy of your personal data previously provided to us in a portable and to the extent technically feasible, readily usable format, thereby permitting you the ability to transmit the data without hindrance.</li>
+                                    <li>5. <b>Right to Opt-Out of Processing:</b> If you submit a valid and authenticated request and we confirm your identity and/or authority to make the request, we will cease processing your personal data for the limited purposes of targeted advertising, sale, and/or profiling. Because we are not selling your personal data for monetary compensation, we do not provide an opt-out for these activities. Since we may engage in targeted advertising and/or profiling, we provide you with the option to opt-out of such processing for these purposes. To do so, please contact our office by phone or email</li>
                                     </ul>		
+                                
                                 <h5><b>Security</b></h5>
-                                <p>We take precautions to protect your information. When you submit sensitive information via the website, your information is protected both online and offline. </p> 
-                                <p>Wherever we collect sensitive information (such as credit card data), that information is encrypted and transmitted to us in a secure way. You can verify this by looking for a closed lock icon at the bottom of your web browser, or looking for "https" at the beginning of the address of the web page. </p>
-                                <p>While we use encryption to protect sensitive information transmitted online, we also protect your information offline. Only employees who need the information to perform a specific job (for example, billing or customer service) are granted access to personally identifiable information. The computers/servers in which we store personally identifiable information are kept in a secure environment. </p>
+                                <p>
+                                    We maintain security standards and procedures to prevent unauthorized removal or alteration of data.
+                                </p>
+                                <p>
+                                    Wherever we collect sensitive information (such as credit card data), that information is encrypted and transmitted to us in a secure way. You can verify this by looking for a closed lock icon at the bottom of your web browser, or looking for "https" at the beginning of the address of the web page.
+                                </p>
+                                <p>
+                                While we use encryption to protect sensitive information transmitted online, we also protect your information offline. Only employees who need the information to perform a specific job (for example, billing or customer service) are granted access to personally identifiable information.
+                                </p>
+
+
                                 <h5><b>Updates</b></h5>
-                                <p>Our Privacy Policy may change from time to time and all updates will be posted on this page. </p>
+                                <p>Our Privacy Policy may change from time to time and all updates will be posted on this page.</p>
                                 
                                 <h5><b>Application Access</b></h5>
-                                <p>Background Location Access is required in the Driver App as we need to identify the live location of the Drivers due to the fact, we allocate Drivers within a certain radius to jobs, for Restaurants which require Drivers to be in close proximity to the Restaurant to ensure the Food arrives heated to the Customer such as Pizza etc. </p>
-                                <p>Background Location Access is required in the Driver App for the following compulsory reasons; </p>
-                                <ul><li>To identify the exact location of the Drivers within a certain radius to allocate jobs, for restaurants which require Drivers to be in close proximity to the restaurants to ensure the Food arrives at the correct temperature </li>
-                                <li>To enable the customer the right of ‘Live Tracking’ the progress of their parcel delivery while onboard after it has been picked up by the assigned driver. </li>
-                                <li>To enable application to track the exact location of the assigned Driver in the event of any emergency along the route of the delivery so to send another Driver to collect the parcels already onboard and continue the delivery to destination </li>
-                                <li>To enable application to widen the proximity of certain order collections in certain areas based on Drivers live availability. </li>
+                                <p>Background Location Access is required in the Driver App as we need to identify the live location of the Drivers. We allocate Drivers within a certain radius to jobs, for Restaurants which require Drivers to be in close proximity to the Restaurant to ensure the Food arrives heated to the Customer such as Pizza etc.</p>
+                                <p>Background Location Access is required in the Driver App for the following compulsory reasons;</p>
+                                <ul>
+                                    <li>· To identify the exact location of the Drivers within a certain radius to allocate jobs, for restaurants which require Drivers to be in close proximity to the restaurants to ensure the Food arrives at the correct temperature</li>
+                                    <li>· To enable the customer the right of ‘Live Tracking’ the progress of their parcel delivery while onboard after it has been picked up by the assigned driver.</li>
+                                    <li>· To enable application to track the exact location of the assigned Driver in the event of any emergency along the route of the delivery so to send another Driver to collect the parcels already onboard and continue the delivery to destination</li>
+                                    <li>· To enable application to widen the proximity of certain order collections in certain areas based on Drivers live availability.</li>
                                 </ul>
-                                <p>To acknowledge drivers that we are tracking their location we have also added disclosure dialogue. </p>
                                 
-                                
-                                <h5><b>Data Deletion</b></h5>
-                                <p>Accounts on this site can be anonymized or deleted at the users request. Contact our support info@cygnuspay.com group for details. </p>
+                                <p>To acknowledge drivers that we are tracking their location we have also added disclosure dialogue.</p>
+
+                                <p>Delivery App data collected:
+                                    <ul>
+                                        <li>Location: is used for maps to navigate delivery drivers to the location where he can pick up and drop food.</li>
+                                        <li>Email (collect and display): For login purpose</li>
+                                        <li>Phone number (only display): For calling</li>
+                                        <li>Vehicle detail (only display): For customers</li>
+                                        <li>Image (collect and display): For displaying in profile and collecting image when order is delivered at doorstep.</li>
+                                    </ul>
+                                </p>
+
+                                <p>Customer iOS and Android Apps data collected:
+                                    <ul>
+                                        <li>Location: is used for maps to let customers track the order.</li>
+                                        <li>Image (collect and display): for displaying to the merchant and delivery driver.</li>
+                                        <li>Phone number (collect and display): For contact regarding order</li>
+                                        <li>Name (collect and display): For displaying to the merchant</li>
+                                        <li>Email (collect and display): For login and sending order information/invoice etc</li>
+                                        <li>Image (collect and display): For displaying</li>
+                                        <li>Address: Uses precise location</li>
+                                    </ul>
+                                </p>
+
+                                <p>Merchant App data collected
+                                    <ul>
+                                        <li>Phone number (only display): For contact regarding order</li>
+                                        <li>Name (only display): For Displaying to the merchant</li>
+                                        <li>Email (only display): Sending order information/invoice etc</li>
+                                    </ul>
+                                </p>
+
+                                <p>Customer Online Ordering website data collected:
+                                    <ul>
+                                        <li>Image (collect and display): for displaying to the merchant and delivery driver.</li>
+                                        <li>Phone number (collect and display): For contact regarding order</li>
+                                        <li>Name (collect and display): For displaying to the merchant</li>
+                                        <li>Email (collect and display): For login and sending order information/invoice etc</li>
+                                        <li>Image (collect and display): For displaying</li>
+                                        <li>Address: For order delivery</li>
+                                    </ul>
+                                </p>
+        
+                                <p>Anyone with access to the above data collected is required by Cygnus Payment Solutions to follow this Privacy Policy. We do not permit any merchant and/or affiliate to sell or rent any data collected using the applications owned by Cygnus Payment Solutions, including any administrative site provided.</p>
                                 
                                 <h5><b>App Removal</b></h5>
                                 <p>Any apps authorized for logging users onto the site can be removed by going to the Associated Accounts section of a user’s preferences page. </p>
